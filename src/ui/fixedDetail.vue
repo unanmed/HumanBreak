5379dc99e6c543f78297b3e1478a41b28cc506a9
unanmed <1319491857@qq.com>
2023-01-06 23:00:38 +0800

    修复定点查看的bug

--- src/ui/fixedDetail.vue
+++ src/ui/fixedDetail.vue
@@ -11,20 +11,21 @@ import BookDetail from './bookDetail.vue';
 
 core.plugin.bookDetailPos = 0;
 
-function close() {
-    core.plugin.fixedDetailOpened.value = false;
-}
-
-onMounted(() => {
-    const [x, y] = flags.mouseLoc;
-    const mx = Math.round(x + core.bigmap.offsetX / 32);
-    const my = Math.round(y + core.bigmap.offsetY / 32);
-    const e = core.getBlockId(mx, my);
-    if (!e || !core.getClsFromId(e)?.startsWith('enemy')) return;
+const [x, y] = flags.mouseLoc;
+const mx = Math.round(x + core.bigmap.offsetX / 32);
+const my = Math.round(y + core.bigmap.offsetY / 32);
+const e = core.getBlockId(mx, my);
+if (e && core.getClsFromId(e)?.startsWith('enemy')) {
     const enemy = core.material.enemys[e as EnemyIds];
     const detail = getDetailedEnemy(enemy, mx, my);
     core.plugin.bookDetailEnemy = detail;
-});
+} else {
+    close();
+}
+
+function close() {
+    core.plugin.fixedDetailOpened.value = false;
+}
 </script>
 
 <style lang="less" scoped>
