90f37f4f66d40fc520dea900668009412f05b31d
unanmed <90094606+unanmed@users.noreply.github.com>
2023-02-28 12:41:58 +0000

    Deploying to gh-pages from  @ 25ff6c64620be5d1cf90ca8c9ab806b9aa7a0c13 🚀

--- project/plugins.js
+++ project/plugins.js
@@ -2,7 +2,7 @@ var plugins_bb40132b_638b_4a9f_b028_d3fe47acc8d1 = {
     init: function () {
         // 这看不到插件，插件全放到plugin文件夹里面了，要看的话去 关于游戏 的开源地址里面看
         // 直接把仓库clone下来，或者下载zip都行
-        // 脚本编辑同理
+        // 脚本编辑同理，一部分在plugin里面
         this._afterLoadResources = function () {};
     }
 };
