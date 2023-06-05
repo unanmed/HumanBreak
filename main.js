f3ce0b80bec0bc6cea61665509c50cb8e8311ef0
unanmed <90094606+unanmed@users.noreply.github.com>
2023-06-05 12:37:25 +0000

    Deploying to gh-pages from  @ 7cc7fe375be6d4023d9c46bb0e77fd00e669fda9 🚀

--- main.js
+++ main.js
@@ -217,11 +217,11 @@ function main() {
     this.__VERSION__ = '2.10.0';
     this.__VERSION_CODE__ = 510;
 
-    this.timestamp = 1685935254343;;
+    this.timestamp = 1685968619608;;
 
     // 远程资源地址，在线游戏中，塔本体不包含任何资源，只包含源码，从而可以降低游戏本体的体积并平均分担资源包体积
     // 从而可以优化加载并避免网站发布的大小限制
-    this.USE_RESORCE = false;
+    this.USE_RESOURCE = false;
     this.RESOURCE_URL = '';
     this.RESOURCE_SYMBOL = '';
     this.RESOURCE_INDEX = {};
