67d425d4ab559655c732cd83568a05e0549c4b5a
unanmed <1319491857@qq.com>
2024-10-05 23:23:54 +0800

    feat: 缓动函数加法与乘法

--- src/core/render/utils.ts
+++ src/core/render/utils.ts
@@ -1,3 +1,4 @@
+import { TimingFn } from 'mutate-animate';
 import { RenderAdapter } from './adapter';
 import { FloorViewport } from './preset/viewport';
 
@@ -11,4 +12,18 @@ export function enableViewport() {
     const adapter = RenderAdapter.get<FloorViewport>('viewport');
     if (!adapter) return;
     adapter.sync('disable');
+}
+
+/**
+ * 将两个缓动函数做加法
+ */
+export function AddTiming(timing1: TimingFn, timing2: TimingFn): TimingFn {
+    return (p: number) => timing1(p) + timing2(p);
+}
+
+/**
+ * 将两个缓动函数做乘法
+ */
+export function multiplyTiming(timing1: TimingFn, timing2: TimingFn): TimingFn {
+    return (p: number) => timing1(p) * timing2(p);
 }
