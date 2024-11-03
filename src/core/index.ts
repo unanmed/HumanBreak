891763387aa31ddde80b1cfcf4e99b184af28c3f
hunhou001 <55705194+hunhou001@users.noreply.github.com>
2024-11-03 21:56:39 +0800

    feat: 雪花（半成品）#1

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,5 @@
+import { WeatherController } from './../module/weather/weather';
+import '@/module/weather/snow';
 import { BgmController, bgm } from './audio/bgm';
 import { SoundController, SoundEffect, sound } from './audio/sound';
 import { Focus, GameUi, UiController } from './main/custom/ui';
@@ -174,3 +176,8 @@ Mota.register('module', 'Animation', Animation);
 
 main.renderLoaded = true;
 Mota.require('var', 'hook').emit('renderLoaded');
+
+const weather = new WeatherController();
+Mota.require('var', 'hook').once('reset', () => {
+    weather.activate('snow');
+});
