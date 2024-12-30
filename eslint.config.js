0c399a03fe70beb9f7e25f57d8c33b86f6dfe14b
unanmed <1319491857@qq.com>
2024-12-30 22:34:17 +0800

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginVue from 'eslint-plugin-vue';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+    {
+        ignores: ['node_modules', 'dist', 'public']
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...eslintPluginVue.configs['flat/recommended'],
+    {
+        files: ['**/*.{js,mjs,cjs,vue}'],
+        rules: {
+            'no-console': 'warn'
+        }
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                wx: true
+            }
+        }
+    },
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            parserOptions: {
+                parser: tseslint.parser,
+                ecmaVersion: 'latest',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        },
+        rules: {
+            'vue/no-mutating-props': [
+                'error',
+                {
+                    shallowOnly: true
+                }
+            ]
+        }
+    },
+    {
+        files: ['**/*.{ts,tsx,vue}'],
+        rules: {
+            '@typescript-eslint/no-empty-object-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true
+                }
+            ]
+        }
+    },
+    eslintPluginPrettierRecommended
+);
