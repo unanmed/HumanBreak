a058dfda4a8ddf2e8bb5d454a9548ce6102c19c0
unanmed <1319491857@qq.com>
2025-01-15 23:14:54 +0800

    feat: 根据支持情况选择播放方式

--- src/module/audio/bgmLoader.ts
+++ src/module/audio/bgmLoader.ts
@@ -1,6 +1,6 @@
 import { StreamLoader } from '../loader';
 import { audioPlayer, AudioRoute } from './player';
-import { AudioType, guessTypeByExt } from './support';
+import { guessTypeByExt, isAudioSupport } from './support';
 
 export function loadAllBgm() {
     const loading = Mota.require('var', 'loading');
@@ -8,17 +8,17 @@ export function loadAllBgm() {
         const data = data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d;
         for (const bgm of data.main.bgms) {
             const type = guessTypeByExt(bgm);
-
-            if (type === AudioType.Opus || type === AudioType.Ogg) {
-                const source = audioPlayer.createStreamSource();
-                const stream = new StreamLoader(`project/bgms/${bgm}`);
-                stream.pipe(source);
+            if (!type) continue;
+            if (isAudioSupport(type)) {
+                const source = audioPlayer.createElementSource();
+                source.setSource(`project/bgms/${bgm}`);
                 source.setLoop(true);
                 const route = new AudioRoute(source, audioPlayer);
                 audioPlayer.addRoute(`bgms.${bgm}`, route);
             } else {
-                const source = audioPlayer.createElementSource();
-                source.setSource(`project/bgms/${bgm}`);
+                const source = audioPlayer.createStreamSource();
+                const stream = new StreamLoader(`project/bgms/${bgm}`);
+                stream.pipe(source);
                 source.setLoop(true);
                 const route = new AudioRoute(source, audioPlayer);
                 audioPlayer.addRoute(`bgms.${bgm}`, route);
