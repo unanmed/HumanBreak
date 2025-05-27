6a7b58cb5eef6fa27ca535accb50460cf5007a24
unanmed <1319491857@qq.com>
2025-05-27 15:56:00 +0800

    chore: 删除一个 log

--- packages-user/client-modules/src/render/components/textboxTyper.ts
+++ packages-user/client-modules/src/render/components/textboxTyper.ts
@@ -956,8 +956,6 @@ export class TextContentParser {
      * @param breakIndex 当前分词的索引
      */
     private checkLineWidth(width: number, guess: number, pointer: number) {
-        console.log(guess);
-
         const breakIndex = this.wordBreak.length - 1;
         if (breakIndex === -1) return true;
         const rest = width - this.lineWidth;
