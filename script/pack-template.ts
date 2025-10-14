8b6dc81ee12fae9eb5028e31bb14e62259eba090
unanmed <1319491857@qq.com>
2025-10-14 19:00:48 +0800

    feat: 添加一个启动服务说明文件

--- script/pack-template.ts
+++ script/pack-template.ts
@@ -1,9 +1,13 @@
 /* eslint-disable no-console */
 import { copy, emptyDir, ensureDir } from 'fs-extra';
+import { writeFile } from 'fs/promises';
 import { resolve } from 'path';
 
 const base = resolve(process.cwd());
 const template = resolve(base, 'template');
+
+const serve = `在 2.B 样板中，不再使用先前的启动服务，而使用一个单独的软件。
+你可以加造塔群 959329661 后，在群文件 - 启动服务 中获取，文件夹中会有一个专门的安装教程。`;
 
 async function packTemplate() {
     await ensureDir(template);
@@ -40,6 +44,8 @@ async function packTemplate() {
             })
         )
     );
+
+    await writeFile(resolve(template, '启动服务呢？.txt'), serve, 'utf-8');
 
     console.log(`样板打包完成`);
 }
