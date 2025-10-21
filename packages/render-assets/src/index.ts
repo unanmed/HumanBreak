f496175bb241888a884367a02e444a9cda46031b
unanmed <1319491857@qq.com>
2025-10-21 22:00:40 +0800

    fix: streamComposer 未导出

--- packages/render-assets/src/index.ts
+++ packages/render-assets/src/index.ts
@@ -2,6 +2,7 @@ export * from './animater';
 export * from './composer';
 export * from './splitter';
 export * from './store';
+export * from './streamComposer';
 export * from './texture';
 export * from './types';
 export * from './utils';
